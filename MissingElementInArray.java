9b3fff5a573316f4a8444c161e9d9cde3fd5757e
Aravindhan <100990609+Aravindhanp11@users.noreply.github.com>
2022-03-10 12:33:38 +0530

    Add files via upload

---
+++ MissingElementInArray.java
@@ -0,0 +1,18 @@
+package week3day1;
+
+public class MissingElementInArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] arr= {1,2,3,4,5,6,7,8};
+		
+		for(int i = 0; i <= arr.length; i++) { 
+			if (i!=arr[i+1]) { 
+			System.out.println(i);	
+			break;	
+			}
+		}
+	}
+
+}
